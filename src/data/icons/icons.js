38f1218695dd26af553037c987f1504c85baa893
Edwin Moradian <edwin.moradian90@gmail.com>
2022-05-18 21:12:34 +0400

    update projects page

--- src/data/icons/icons.js
+++ src/data/icons/icons.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { FaAngellist, FaLinkedin, FaGithub, FaMedium } from "react-icons/fa";
-import { MdEmail, MdDateRange } from "react-icons/md";
-import { SiFirebase, SiRedux } from "react-icons/si";
-import { GiHamburgerMenu } from "react-icons/gi";
+import React from 'react';
+import { FaAngellist, FaLinkedin, FaGithub, FaMedium } from 'react-icons/fa';
+import { MdEmail, MdDateRange } from 'react-icons/md';
+import { SiFirebase, SiRedux } from 'react-icons/si';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 const icons = {
   angellist: <FaAngellist />,
@@ -15,56 +15,60 @@ const icons = {
   redux: <SiRedux />,
   calendar: <MdDateRange />,
   bootstrap: {
-    colored: <i class="devicon-bootstrap-plain colored"></i>,
-    plain: <i class="devicon-bootstrap-plain"></i>,
+    colored: <i class='devicon-bootstrap-plain colored'></i>,
+    plain: <i class='devicon-bootstrap-plain'></i>,
   },
   css: {
-    colored: <i class="devicon-css3-plain colored"></i>,
-    plain: <i class="devicon-css3-plain"></i>,
+    colored: <i class='devicon-css3-plain colored'></i>,
+    plain: <i class='devicon-css3-plain'></i>,
   },
   heroku: {
-    colored: <i class="devicon-heroku-plain colored"></i>,
-    plain: <i class="devicon-heroku-plain"></i>,
+    colored: <i class='devicon-heroku-plain colored'></i>,
+    plain: <i class='devicon-heroku-plain'></i>,
   },
   html: {
-    colored: <i class="devicon-html5-plain colored"></i>,
-    plain: <i class="devicon-html5-plain"></i>,
+    colored: <i class='devicon-html5-plain colored'></i>,
+    plain: <i class='devicon-html5-plain'></i>,
   },
   javascript: {
-    colored: <i class="devicon-javascript-plain colored"></i>,
-    plain: <i class="devicon-javascript-plain"></i>,
+    colored: <i class='devicon-javascript-plain colored'></i>,
+    plain: <i class='devicon-javascript-plain'></i>,
   },
   mongodb: {
-    colored: <i class="devicon-mongodb-plain colored"></i>,
-    plain: <i class="devicon-mongodb-plain"></i>,
+    colored: <i class='devicon-mongodb-plain colored'></i>,
+    plain: <i class='devicon-mongodb-plain'></i>,
   },
   node: {
-    colored: <i class="devicon-nodejs-plain colored"></i>,
-    plain: <i class="devicon-nodejs-plain"></i>,
+    colored: <i class='devicon-nodejs-plain colored'></i>,
+    plain: <i class='devicon-nodejs-plain'></i>,
   },
   postgresql: {
-    colored: <i class="devicon-postgresql-plain colored"></i>,
-    plain: <i class="devicon-postgresql-plain "></i>,
+    colored: <i class='devicon-postgresql-plain colored'></i>,
+    plain: <i class='devicon-postgresql-plain '></i>,
   },
   rails: {
-    colored: <i class="devicon-rails-plain colored"></i>,
-    plain: <i class="devicon-rails-plain colored"></i>,
+    colored: <i class='devicon-rails-plain colored'></i>,
+    plain: <i class='devicon-rails-plain colored'></i>,
   },
   react: {
-    colored: <i class="devicon-react-original colored"></i>,
-    plain: <i class="devicon-react-original"></i>,
+    colored: <i class='devicon-react-original colored'></i>,
+    plain: <i class='devicon-react-original'></i>,
   },
   ruby: {
-    colored: <i class="devicon-ruby-plain-wordmark colored"></i>,
-    plain: <i class="devicon-ruby-plain-wordmark"></i>,
+    colored: <i class='devicon-ruby-plain-wordmark colored'></i>,
+    plain: <i class='devicon-ruby-plain-wordmark'></i>,
+  },
+  typescript: {
+    colored: <i class='devicon-typescript-plain colored'></i>,
+    plain: <i class='devicon-typescript-plain'></i>,
   },
   sass: {
-    colored: <i class="devicon-sass-original colored"></i>,
-    plain: <i class="devicon-sass-original"></i>,
+    colored: <i class='devicon-sass-original colored'></i>,
+    plain: <i class='devicon-sass-original'></i>,
   },
   webpack: {
-    colored: <i class="devicon-webpack-plain colored"></i>,
-    plain: <i class="devicon-webpack-plain"></i>,
+    colored: <i class='devicon-webpack-plain colored'></i>,
+    plain: <i class='devicon-webpack-plain'></i>,
   },
 };
 
--- src/data/projects/projects.js
+++ src/data/projects/projects.js
@@ -1,88 +1,66 @@
-import tracker from "../../assets/images/projects/tracker.png";
-import newsweek from "../../assets/images/projects/newsweek.jpg";
-import bookstore from "../../assets/images/projects/bookstore.png";
-import facebook from "../../assets/images/projects/fakebook.png";
-import calculator from "../../assets/images/projects/calculator.png";
-import ecommerce from "../../assets/images/projects/ecommerce.jpg";
-import astro from "../../assets/images/projects/astro.png";
+import tracker from '../../assets/images/projects/tracker.png';
+import newsweek from '../../assets/images/projects/newsweek.jpg';
+import facebook from '../../assets/images/projects/fakebook.png';
+import astro from '../../assets/images/projects/astro.png';
+import gumar from '../../assets/images/projects/gumar-screenshot-wide.jpg';
 
 export default [
   {
-    id: "trackerapp",
-    name: "Tracker App",
-    image: tracker,
-    github: "https://github.com/edwinmoradian90/tracker-app",
-    url: "https://tracker-app-react-rails.herokuapp.com",
+    id: 'gumar',
+    name: 'Gumar',
+    image: gumar,
+    github: 'https://github.com/edwinmoradian90/gumar',
+    url: 'https://vimeo.com/711234278',
     description:
-      "A tracking app that trackers amount driven, fuel usage, and driving limit. Built with React, Redux, and Rails.",
+      'A finance management tool that tracks transactions and exports data directly to Google sheets. Built with React Native, Redux, and Typescript.',
+    tools: ['react', 'redux', 'typescript'],
+  },
+  {
+    id: 'game',
+    name: 'Space Shooter',
+    image: astro,
+    github: 'https://github.com/edwinmordian/runner',
+    url: 'https://bit.ly/36aWcVm',
+    description:
+      'A space shooter built with JavaScript! Just float around and shoot the asteroids.',
+    tools: ['html', 'css', 'javascript'],
+  },
+  {
+    id: 'newsweek',
+    name: 'NewsWeek Clone',
+    image: newsweek,
+    github: 'https://github.com/edwinmoradian90/newsweek-clone-bootstrap',
+    url: 'https://raw.githack.com/edwinmoradian90/newsweek-clone-bootstrap/master/index.html',
+    description:
+      "A NewsWeek clone built to replicate the popular news outlet, 'NewsWeek'. Built with HTML, CSS, and Bootstrap 4.",
+    tools: ['html', 'css', 'bootstrap'],
+  },
+  {
+    id: 'trackerapp',
+    name: 'Tracker App',
+    image: tracker,
+    github: 'https://github.com/edwinmoradian90/tracker-app',
+    url: 'https://tracker-app-react-rails.herokuapp.com',
+    description:
+      'A tracking app that trackers amount driven, fuel usage, and driving limit. Built with React, Redux, and Rails.',
     tools: [
-      "rails",
-      "react",
-      "redux",
-      "postgresql",
-      "sass",
-      "heroku",
-      "webpack",
+      'rails',
+      'react',
+      'redux',
+      'postgresql',
+      'sass',
+      'heroku',
+      'webpack',
     ],
   },
   {
-    id: "newsweek",
-    name: "NewsWeek Clone",
-    image: newsweek,
-    github: "https://github.com/edwinmoradian90/newsweek-clone-bootstrap",
-    url:
-      "https://raw.githack.com/edwinmoradian90/newsweek-clone-bootstrap/master/index.html",
-    description:
-      "A NewsWeek clone built to replicate the popular news outlet, 'NewsWeek'. Built with HTML, CSS, and Bootstrap 4.",
-    tools: ["html", "css", "bootstrap"],
-  },
-  {
-    id: "game",
-    name: "Space Shooter",
-    image: astro,
-    github: "https://github.com/edwinmordian/runner",
-    url: "https://bit.ly/36aWcVm",
-    description: "A space shooter built with JavaScript! Just float around and shoot the asteroids.",
-    tools: ["html", "css", "javascript"]
-  },
-  {
-    id: "bookstore",
-    name: "Bookstore CMS",
-    image: bookstore,
-    github: "https://github.com/edwinmoradian90/bookstore-cms",
-    url: "https://microverse-bookstore.herokuapp.com",
-    description:
-      "A book store CMS that handles and tracks books read. Built with React, Redux, and Styled Components.",
-    tools: ["rails", "react", "redux", "postgresql", "sass", "heroku"],
-  },
-  {
-    id: "facebook",
-    name: "Facebook Clone",
+    id: 'facebook',
+    name: 'Facebook Clone',
     image: facebook,
-    github: "https://github.com/edwinmoradian90/facebook-clone",
-    url: "https://shielded-taiga-48885.herokuapp.com",
+    github: 'https://github.com/edwinmoradian90/facebook-clone',
+    url: 'https://shielded-taiga-48885.herokuapp.com',
     description:
-      "A FaceBook clone with login, friends and friend requests, timeline, and more. Built with Ruby and Rails.",
-    tools: ["rails", "postgresql", "sass", "heroku"],
-  },
-  {
-    id: "calculator",
-    name: "Calculator",
-    image: calculator,
-    github: "https://github.com/edwinmoradian90/calculator",
-    url: "https://calculator-a6ddd.web.app",
-    description:
-      "A calculator with all adding, subtracting, dividing, and multiplying functionality. Built with React.",
-    tools: ["react", "css", "firebase"],
-  },
-  {
-    id: "eccommerce",
-    name: "Ecommerce App",
-    image: ecommerce,
-    github: "https://github.com/edwinmoradian90/ecommerce_webapp",
-    url: "https://stark-coast-27283.herokuapp.com",
-    description:
-      "My first project, an ecommerce application with login, checkout, payment, and more. Built with React and Nodejs.",
-    tools: ["react", "node", "mongodb", "bootstrap", "heroku"],
+      'A FaceBook clone with login, friends and friend requests, timeline, and more. Built with Ruby and Rails.',
+    tools: ['rails', 'postgresql', 'sass', 'heroku'],
   },
 ];
